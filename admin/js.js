519ede6131fd6c6f43e977658517fde536770ef6
Javierdz <javiusgs@gmail.com>
2024-01-11 21:24:17 +0100

    Create js.js

---
+++ admin/js.js
@@ -0,0 +1,8 @@
+//si funciona soy dios
+var user = document.getElementById('username');
+var passwd = document.getElemetById('password');
+var submit = document.getElemetById('submit');
+
+function clicked(){
+  window.location.replace("http://iabillanos.tv/video.mp4");
+}

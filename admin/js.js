6fc38399d3e3619eaa135b84abd818c9b2b68907
Javierdz <javiusgs@gmail.com>
2024-01-11 21:27:56 +0100

    Update js.js

--- admin/js.js
+++ admin/js.js
@@ -1,7 +1,7 @@
 //si funciona soy dios
 var user = document.getElementById('username');
-var passwd = document.getElemetById('password');
-var submit = document.getElemetById('submit');
+var passwd = document.getElementById('password');
+var submit = document.getElementById('submit');
 
 function clicked(){
   window.location.replace("http://iabillanos.tv/video.mp4");

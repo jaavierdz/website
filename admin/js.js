c5110870117068dfa61ae7b70a4768a25e0976d6
Javierdz <javiusgs@gmail.com>
2024-01-19 21:17:06 +0100

    Update js.js

--- admin/js.js
+++ admin/js.js
@@ -4,6 +4,5 @@ var passwd = document.getElementById('password');
 var submit = document.getElementById('submit');
 
 function clicked(){
-  if (username == "admin" && password == "admin"){
-    window.location.replace("http://ibaillanos.tv/video.mp4");
-  } else alert("Invalid credentials");
+  window.location.replace("https://javierdz.xyz/admin/video.mp4")
+}
